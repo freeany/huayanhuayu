76aa314133b9d1ac49602981bd7978ea194e137a
李浩然 <lhr_freeany@163.com>
2024-08-14 19:52:21 +0800

    通用模块封装

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,1 +1,13 @@
-App({})
+// 执行 extendApi.js 文件，将方法挂载到 wx 全局对象身上
+import "./utils/extendApi";
+
+App({
+  async onLaunch() {
+    // 第二种调用方式：更改默认配置
+    // const res = await wx.modal({
+    //   content: "鉴权失败，请重新登录",
+    //   showCancel: false,
+    // });
+    // console.log(res);
+  },
+});
